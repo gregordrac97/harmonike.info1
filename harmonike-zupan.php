4637d0f65ad4240b9c57ff6181e2e87e59640f27
gregordrac97 <47061182+gregordrac97@users.noreply.github.com>
2019-05-20 14:45:12 +0200

    Add files via upload

---
+++ harmonike-zupan.php
@@ -0,0 +1,514 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="zupan-style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<style>
+	
+	@media (min-width: 900px) {
+		#content {
+			display: ;
+		}
+	}
+	
+	@media (max-width: 900px) {
+		.side {
+			width: 35%;
+		}
+		.main {
+			width: 60%;
+			float: left;
+		}
+		#content {
+			display: ;
+		}
+	}
+	
+	@media (max-width: 700px) {
+		img.img1 {
+			position: relative;
+			width: 300px;
+		}
+		img.zacetna-img {
+			width: 250px;
+		}
+		img.himna-img {
+			width: 300px;
+		}
+		.ursplet {
+			float: left
+		}
+		.responsive {
+			width: 50%;
+		}
+		#content {
+			display: none;
+		}
+	}
+	
+	@media (max-width: 600px) {
+		.side {
+			display: none;
+		}
+		.main {
+			width: 100%;
+		}
+		img.himna-img {
+			width: 200px;
+		}
+		#content {
+			display: flex;
+		}
+	}
+	
+</style>
+</head>
+<body>
+	
+<div class="header">
+	<div class="logo">
+		<img src="harmonike.info-logo.png">
+	</div>
+</div>
+	
+<div class="icons">
+	<a href="https://www.facebook.com/Harmonike.info/" class="facebook"><i class="fab fa-facebook-f"></i>HARMONIKE.INFO</a>
+</div>
+	
+<div class="navbar">
+	<a href="#" id="content"><i class="fas fa-align-justify"></i></a>
+</div>
+	
+<div class="bg-modal">
+	<div class="modal-contents">
+
+		<div class="close">+</div>
+		<h3 style="text-align: center;">Na harmoniko Zupan igrajo:</h3><br>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Miha Debevec</a>
+		</div>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Toni Iskra</a>
+		</div>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Jodel Expres</a>
+		</div>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Laurent Africano</a>
+		</div>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Kathy Hlad</a>
+		</div>
+		<hr>
+		<div class="izdelovalci">
+			<a href="#">Lenti Lenko</a>
+		</div>
+		<hr>
+
+	</div>
+</div>
+
+	<script>
+		document.getElementById('content').addEventListener("click", function() {
+			document.querySelector('.bg-modal').style.display = "flex";
+		});
+
+		document.querySelector('.close').addEventListener("click", function() {
+			document.querySelector('.bg-modal').style.display = "none";
+		});
+	</script>
+	
+<div class="side">
+	<div class="izdelovalci">
+		<a href="harmonike1.html"><i class="fas fa-arrow-left"></i> NAZAJ</a>
+	</div>
+
+	<br>
+	<h3 style="text-align: center;">Na harmoniko Zupan igrajo:</h3><br>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Miha Debevec</a>
+	</div>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Toni Iskra</a>
+	</div>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Jodel Expres</a>
+	</div>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Laurent Africano</a>
+	</div>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Kathy Hlad</a>
+	</div>
+	<hr>
+	<div class="izdelovalci">
+		<a href="#">Lenti Lenko</a>
+	</div>
+	<hr>
+</div>
+
+<div class="main">
+	<div align="center">
+		<a class="logo1" href="#"><img class="img1" src="zupan-logo-1.png" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="200"></a>
+	</div>
+	
+	<h1 align="center">HIŠA HARMONIKE ZUPAN</h1>
+	
+	<h2 align="center">IME S TRADICIJO ZNANJA,<br> KVALITETE IN ZANESLJIVOSTI</h2>
+	
+	<div align="center">
+		<a class="zacetna-a" target="_blank" href="zupan-zacetna.jpg">
+			<img class="zacetna-img" src="zupan-zacetna.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="320">
+		</a>
+		<p>VERJETNOST V LASTNO ZMAGO JE VELIKA, <br>ČE VERJAMETE V LASTNE SANJE !</p>
+	</div><br><br>
+
+	<div align="center">
+		<a class="medalje-a" target="_blank" href="zupan-medalje-1.jpg">
+			<img class="medalje-img" src="zupan-medalje-1.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="283">
+		</a>
+		<a class="medalje-a" target="_blank" href="zupan-medalje-2.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="283">
+			<img class="medalje-img" src="zupan-medalje-2.jpg">
+		</a>
+	</div><br><br>
+	
+	<h3 align="center">ZAKAJ HARMONIKA ZUPAN ?</h3>
+	<p align="center">Mnogo vas je, ki se zanimate po telefonu, emailih ali preko FB zasebnih sporočil za diatonične in klavirske harmonike ZUPAN, ter skoraj vedno postavljate tudi vprašanja, v čem je harmonika ZUPAN boljša ali drugačna od drugih znamk.</p>
+	<h5 style="font-size: 15px;">Dragi naši bodoči kupci:</h5>
+	<p>Vljudno vas usmerjamo na povezavo <a href="https://harmonikazupan.com/who-we-are/nagrade-patenti/">(klikni tukaj)</a>, kjer lahko dobite vse informacije o naših dosežkih v razvoju.<br>
+	Predlagamo pa vam, da lahko povprašate pri drugih proizvajalcih koliko in katere nagrade ter priznanja za razvoj in inovativnost so pridobili ter na ta način primerjate kakovost in zaupanje v blagovno znamko.<br>
+	Mi vam lahko in bomo z veseljem svetovali in povedali vse o harmonikah ZUPAN, saj le te poznamo od samega začetka.<br><br>
+	Vljudno vabljeni v našo hišo tradicije - hišo harmonike ZUPAN</p><br>
+	
+	<h3 align="center">HARMONIKE ZUPAN</h3>
+	<p>Hiša harmonike ZUPAN vključuje razvoj, proizvodnjo, prodajo in servis harmonik, glasbeno galerijo s 350 ročno izdelanimi lesenimi intarzijami, pojočimi stopnicami in slikami z glasbenimi motivi, muzej starih harmonik in arhiv dokumentov o najstarejših harmonikah na Slovenskem.</p>
+	
+	<h3 align="center">VALENTIN ZUPAN</h3>
+	<p>Po poklicu izdelovalec lesenih glasbenih instrumentov. Svojo kariero neslutenih razsežnosti je začel leta 1951 kot učenec v tovarni glasbil Mengeš. Prvo harmoniko je za šolsko razstavo izdelal pri rosnih 16.letih. Po 29. Letih dela v tovarni se je odločil, da se bo popolnoma posvetil razvoju, izboljševanju in izdelovanju povsem lastnih modelov glasbil.<br>Zaradi nenaklonjenosti takratnih političnih oblasti, je leta 1980 lastno obrt preselil v sosednjo Italijo, kjer je lahko 18. let nemoteno nadaljeval svoje poslanstvo. Valentin Zupan je eden redkih proizvajalcev harmonik, ki po svojih zamislih sam nariše načrte, naredi maketo in se potem loti izdelave vzorca. Vse, kar je v svojem življenju ustvaril, je plod njegovih zamisli in rok. V osmih desetletjih svojega življenja njegovo delo še vedno dosega vrhunce. Harmonike razvija in izdeluje v domačem Mengšu. Zupanove harmonike so si zaradi svoje kakovosti in izvirnosti pridobile velik ugled in dobro ime tako doma kot po celem svetu.</p>
+	
+	<h3 align="center">ZUPANOVA HIMNA</h3>
+	<div align="center">
+		<a class="himna-a" target="_blank" href="zupan-himna.png">
+			<img class="himna-img" src="zupan-himna.png" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="600">
+		</a>
+	</div>
+	<h5 style="font-size: 15px;">Dragi naši!</h5>
+	<p>Kot veste ali pa tudi še ne, da ima harmonika ZUPAN svojo skladbo, ki jo imamo v naši družini za "himno" katere avtor je Valentin Zupan.<br>Naslov skladbe posvečene harmoniki ZUPAN je – ZUPAN HARMONIKA HIMNA.<br><br> Objavljamo povezavo na katero lahko kliknete ter z nadaljnjimi koraki pridete do možnosti, da se lahko skladbo naučite v celoti samostojno.<br>Želimo vam veliko zabave ob učenju in spoznavanju online " učitelja " Vharmonika - virtualni učitelj igranja na harmoniko na povezavi:</p> 
+	<ul>
+		<li><a href="http://www.vharmonika.com/index.php…">3-vrstne harmonike</a></li>
+		<li><a href="http://www.vharmonika.com/index.php…">4-reihige</a></li>
+		<li><a href="http://www.vharmonika.com/index.php…">Klavirska harmonika</a></li>
+	</ul><br><br>
+	
+	<div class="ursplet">
+		<a href="#">URADNA SPLETNA STRAN HIŠE HARMONIKE ZUPAN</a>
+	</div>
+	
+	<h3>GALERIJA</h3>
+	<h5 style="font-size: 15px;"><i><ins>IZ ZAKLADNICE HIŠE HARMONIKE ZUPAN – SLAK ZUPAN</ins></i></h5>
+	<p>Unikat, ki nima para ! Izdelek, ki izžareva ponos !<br><br>TA EDINSTVENA HARMONIKA JE DRAGOCENOST, KI SI JO LAHKO IZBERETE ZASE !<br>Je harmonika, ob kateri boste zasijali tudi sami, z vsem sijajem!<br>Ona je izdelek, ob katerem smo tudi sami hvaležni za nam lastno znanje!<br>Po 47. letih se je v hišo vrnila harmonika SLAK ZUPAN.<br>Neizmerna veselje in sreča nas prevevata ob tem, da smo jo znova lahko prijeli v roke.</p>
+	
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-zaklad.jpg">
+				<img src="zupan-zaklad.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	
+	<div class="vmesna"></div>
+	<hr>
+	
+	<p>* VINTAGE - ko nase, kaj daš !</p>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-vintage.jpg">
+				<img src="zupan-vintage.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan17.jpg">
+				<img src="zupan17.jpg" alt="Oooops, slike ni bilo mogoče najti :)" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	
+	<div class="vmesna"></div>
+	<hr>
+	
+	<p>* VANESSA - HARMONIKA NOVE GENERACIJE - zvok, ki očara !</p>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan25.jpg">
+				<img src="zupan25.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan26.jpg">
+				<img src="zupan26.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<p>* Legenda med legendami, po celem svetu.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan3.jpg">
+				<img src="zupan3.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan4.jpg">
+				<img src="zupan4.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+	
+	<p>* Bolj ženstvena od moškega ega.</p>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan2.jpg">
+				<img src="zupan2.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan16.jpg">
+				<img src="zupan16.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan8.jpg">
+				<img src="zupan8.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<p>* Lepoto duše lahko merite z lepoto svoje harmonike in njenimi zvoki !</p>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan11.jpg">
+				<img src="zupan11.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<p>* PRIVOŠČI SI - lahko imaš črno na sebi.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan13.jpg">
+				<img src="zupan13.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h5 style="font-size: 15px;">HIŠA HARMONIKE ZUPAN - A TI ENO ZAIGRAM ?</h5>
+
+	<p>DRAGI STARŠI - IZBIRA JE VAŠA !<br><br>V naši ponudbi imamo na voljo modele harmonik s katerimi bodo vaši otroci zadovoljni, Vi pa boste lahko uživali v njihovem igranju - igranju z zadovoljstvom !<br><br>Vabi vas HIŠA HARMONIKE ZUPAN</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-otroske.jpg">
+				<img src="zupan-otroske.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<p>* ELEGANCA, PERFEKCIONIZEM, STVARJENJE MISLI, IDEJA, KONCEPT ... vse to in še več je združeno v instrument s katerim lahko harmonikar doseže odlično nadgradnjo lastnega muziciranja in glasbenega ustvarjanja in užitkov.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-vintage-1.jpg">
+				<img src="zupan-vintage-1.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>CENTER RABLJENIH HARMONIK</h4>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-rabljene-harmonike.jpg">
+				<img src="zupan-rabljene-harmonike.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h5>HIŠA HARMONIKE ZUPAN - IMATE JO DOMA - PLAČUJETE PA PO OBROKIH</h5><br>
+	<p>HITRI KREDIT v 1 uri za RABLJENE HARMONIKE</p><br>
+	<p>Še vedno velja, da lahko tudi rabljene harmonike kupite na obroke. <br>Pri nas uredite vse potrebno za odobritev hitrega kredita pri naši partnerski banki.
+	Kako to izvesti?<br>Preprosto, pokličite nas oz.nas kontaktirajte in bomo skupaj poiskali najbolj ugodno ponudbo za vaš nakup.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-kredit.jpg">
+				<img src="zupan-kredit.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>DIAGNOSTIKA HARMONIK</h4>
+	<h5>HIŠA HARMONIKE ZUPAN - DIAGNOSTIKA HARMONIK<br>
+	DIAGNOSTICIRAMO vse znamke harmonik !</h5><br>
+
+	<p>GENERALNI PREVENTIVNI PREGLED vaše harmonike, tako boste natančno izvedeli v kakšnem stanju je vaša harmonika.<br>Preglede izvaja sam mojster VALENTIN ZUPAN s sodelavci.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-diagnostika.jpg">
+				<img src="zupan-diagnostika.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>SERVIS HARMONIK</h4>
+	<p>Obveščamo vse lastnike diatoničnih harmonik ZUPAN- modelov JUBILEUM, VANESSA, RESONA12, JUBILEUM M/65,da imamo v našem servisu ZUPAN za omenjene modele zagotovljene originalne nadomestne dele.<br>Vabimo Vas, da se v primeru servisa prej omenjenih modelov harmonik oglasite in si zagotovite kvaliteten ter strokovni pregled in posledično menjavo originalnih delov.</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-servis.jpg">
+				<img src="zupan-servis.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-servis-1.jpg">
+				<img src="zupan-servis-1.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-servis-2.jpg">
+				<img src="zupan-servis-2.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan-servis-3.jpg">
+				<img src="zupan-servis-3.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>ERGOLINE JERMENA</h4>
+	<h5 style="font-size: 15px;">ZAVEDANJE JE POL ZDRAVJA- JERMENA" ERGOLINE " !</h5>
+
+	<p>Preverjeno in potrjeno je, da harmonika pušča harmonikarju trajne zdravstvene težave!<br>JERMENA » ERGOLINE« pa razbremenijo hrbtenico, harmonika postane veliko lažja, ne leze z ramen, ,na telesu je stabilna in se vam med igranjem ne premika v levo in desno, kot se vam to sicer redno dogaja.<br>Za vse vrste in vse blagovne znamke harmonik, vam ponuja Hiša harmonike Zupan JERMENA » ERGOLINE«.<br>Preventivno in zato, ker se imate radi, si privoščite jermena ERGOLINE!</p><br>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan20.jpg">
+				<img src="zupan20.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h5 style="font-size: 15px;">* ZUPAN FIX PATENT - SAMO V HIŠI HARMONIKE ZUPAN !</h5>
+
+	<p>Rešitev, ki je na dlani - ZUPAN FIX za vsako težjo harmoniko ali za vsakega, ki si želi olajšati breme svoje hrbtenice.<br>ZUPAN-FIX je odlična podpora za razbremenitev ramenskega dela, ter zgornjega dela hrbtenice.<br>S pomočjo ZUPAN - FIX patenta postane harmonika veliko veliko lažje "breme" za vašo hrbtenico.<br>Patent FIX se lahko montira ter prilagodi na katero koli harmoniko, naj bo klavirska ali kromatična ali diatonična ter katerekoli znamke.<br><br>ZUPAN- FIX je cenejši od kurative hrbtenice .</p>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan24.jpg">
+				<img src="zupan24.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>KOVČKI ZA HARMONIKE</h4><br>
+	<p>V naši pestri ponudbi že vrsto let dodajamo naše lastne proizvode predvsem v smeri kako olajšati bremena harmonikarjev.<br>Zupan kovčki so primer našega dolgoletnega razvoja in vizije. Kovčki imajo vse kar harmonikar potrebuje, da primerno zavaruje harmoniko pred zunanjimi vremenskimi vplivi kot tudi pred morebitnimi udarci oz.poškodbami.<br>Harmonikar si harmoniko lahko "pripelje" celo na kolesckih, ki so vgrajeni v kovcke.</p><br>
+
+	<div class="responsive">
+		<div class="gallery">
+			<a target="_blank" href="zupan22.jpg">
+				<img src="zupan22.jpg" alt="Cinque Terre" width="600" height="400">
+			</a>
+		</div>
+	</div>
+
+	<div class="vmesna"></div>
+	<hr>
+
+	<h4>NAHRBTNIKI</h4>
+
+	<div class="responsive">
+	<div class="gallery">
+		<a target="_blank" href="zupan27.jpg">
+			<img src="zupan27.jpg" alt="Cinque Terre" width="600" height="400">
+		</a>
+	</div>
+	</div>
+</div>
+	
+<div class="footer">
+	<p style="text-align: center;">Created by <a href="http://www.gregordrac.com">GREGOR DRAČ</a></p>
+</div>
+</html>
